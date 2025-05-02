fa77f27a57e3617ed00c31c6a46398a4810a8a66
Humberto <humbertopereira_15@hotmail.com>
2025-05-02 17:46:06 -0300

    ajuste na rota de usuário

--- auth-service/src/presentation/routes/private/usuario.route.ts
+++ auth-service/src/presentation/routes/private/usuario.route.ts
@@ -1,14 +1,16 @@
-import { InserirUsuario } from "../../../aplication/usecases/inserirUsuario"
+import InserirUsuario from "../../../aplication/usecases/inserirUsuario"
 import HTTP from "../../../domain/abstracoes/aHttp"
 import EncrypterBcryptAdapter  from "../../../infra/crypto/encrypterBcryptAdapter"
 import { PostgresAdapter } from "../../../infra/database/postgresAdapter"
 import { UsuarioRepository } from "../../../infra/repositories/usuarioRepository"
+import ZodValidatorAdapter from "../../../infra/validators/zodValidatorAdapter"
 import { UsuarioController } from "../../controllers/usuarioController"
 
 const conexao = PostgresAdapter.getInstance()
 const usuarioRepository = new UsuarioRepository(conexao)
 const encrypter = new EncrypterBcryptAdapter()
-const inserirUsuarioUseCase = new InserirUsuario(usuarioRepository, encrypter)
+const zodValidatorAdapter = new ZodValidatorAdapter()
+const inserirUsuarioUseCase = new InserirUsuario(usuarioRepository, encrypter, zodValidatorAdapter)
 const usuarioController = new UsuarioController(inserirUsuarioUseCase)
 
 export = (servidor: HTTP) => {
